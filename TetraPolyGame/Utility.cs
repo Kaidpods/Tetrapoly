a6d97a88b10e48ce78718ac7cc38c2b41f614f05
Kaidpods <kaidenmclaughlin@yahoo.co.uk>
2024-05-14 11:45:46 +0100

    Added Util and Railroads

---
+++ TetraPolyGame/Utility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TetraPolyGame
+{
+    public class Utility : Card
+    {
+        private double multiplier;
+        private int position;
+        public Utility(double multiplier, string name, int position, int price, int rent, Player owned, bool mortgaged,
+                        int mortgagePrice, int mortgageCost) : base(name, position, price, rent, owned, mortgaged, mortgagePrice, mortgageCost)
+        {
+            this.multiplier = multiplier;
+            multiplier = 1.0;
+        }
+
+        public int GetRollRent(int diceRoll)
+        {
+            return Convert.ToInt32(diceRoll * multiplier);
+        }
+
+        public double GetMult()
+        {
+            return multiplier;
+        }
+    }
+}
