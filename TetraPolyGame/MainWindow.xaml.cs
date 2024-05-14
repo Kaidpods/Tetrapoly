4e691cb3cb15f1a03da6d3867a89cedf4476e693
dawid <dawidmaivan04@wp.pl>
2024-05-14 11:51:00 +0100

    Board added

--- TetraPolyGame/MainWindow.xaml.cs
+++ TetraPolyGame/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -19,11 +20,47 @@ namespace TetraPolyGame
     {
         private MSSQLdataAccess database = new();
         private List<Card> Cards = new();
+        protected List<Player> Players = new();
         public MainWindow()
         {
             InitializeComponent();
             Cards = database.GetProperties();
 
+        }
+        public void checkposition(int turn)
+        {
+            bool t=true;
+            int count = 0;
+            int pos=Players[turn].GetPosition();
+            while (t==true) 
+            {
+                if (pos == Cards[count].)
+                {
+                    int cut = 0;
+                    while (Players[cut]!=null)
+                    {
+                        bool onby = Players[cut].CheckSet(Cards[count]);
+                        if (onby==true) 
+                        {
+                            if (Players[cut] != Players[turn])
+                            {
+
+                            }
+                            if (Players[cut] == Players[turn]) 
+                            {
+                                
+                            }
+                        }
+                        cut = cut + 1;
+                    }
+                    t=false; 
+                }
+                count=count + 1;
+            }
+        }
+        public bool doyouwanttobuy() 
+        { 
+            return true; 
         }
     }
 }
