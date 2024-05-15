b44c44365fc4c1407831563ec253722aad4666d5
Kaidpods <kaidenmclaughlin@yahoo.co.uk>
2024-05-15 13:41:32 +0100

    Added reading in from DB

---
+++ TetraPolyGame/EffectParser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Linq.Expressions;
+using alias = System.Linq.Dynamic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace TetraPolyGame
+{
+    public class EffectParser
+    {
+        public static Action<Player> ParseEffect(string effectDescription)
+        {
+            // Compile the code snippet into a delegate
+            LambdaExpression lambda = alias.DynamicExpression.ParseLambda(typeof(Player), typeof(void), effectDescription);
+            return (Action<Player>)lambda.Compile();
+        }
+    }
+}
