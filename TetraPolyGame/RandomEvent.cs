d9b3cc432383f77887a3c20978179e1c4d7e8902
Daniel Seix <30160064@my.nclan.ac.uk>
2024-05-21 13:17:54 +0100

    Replaced text file with csv, added reading from file in.

--- TetraPolyGame/RandomEvent.cs
+++ TetraPolyGame/RandomEvent.cs
@@ -23,40 +23,19 @@ namespace TetraPolyGame
                 if (randomValue < chancePercentage)
                 {
                     //read from csv file containing unique id for each case, name of event, description of event
+                    List<String> lines = new List<String>();
+                    string line;
+                    System.IO.StreamReader file = new System.IO.StreamReader("resources\\RandomEvents.csv");
+
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                    file.Close();
+
+                    //lines[0];
                 }
             }       
-        }
-
-
-        /// <summary>
-        /// Read data from a file. This works 
-        /// </summary>
-        /// <param name="filename">The name of the file to read.</param>
-        /// <returns>List containing the lines in the file.</returns>
-        /// <exception cref="Exception">Any errors are thrown up to be dealty with.</exception>
-        public static List<string> ReadFromFile(string filename)
-        {
-            List<string> rows = new List<string>();
-            try
-            {
-                using (StreamReader reader = new StreamReader(filename))
-                {
-                    string line;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        rows.Add(line);
-                    }
-                }
-            }
-            catch (FileNotFoundException fnfe)
-            {
-                throw new Exception("Exception thrown: " + fnfe.Message);
-            }
-            catch (IOException ioe)
-            {
-                throw new Exception("Exception thrown: " + ioe.Message);
-            }
-            return rows;
         }
     }
 }
